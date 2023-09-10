9a499b34e62ad9d758de4424fed6d3e8808d6260
ArnaudBvd <bravard.arnaud@gmail.com>
2023-09-10 17:18:02 +0200

    Création d'un service & appel à l'API weathermap.org

---
+++ src/app/weather.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class WeatherService {
+
+  constructor(private http: HttpClient) { }
+
+  getWeather() {
+    return this.http.get('https://api.openweathermap.org/data/2.5/weather?q=lyon&appid=a005739e80529cdd8953b92e4da1cce2&units=metric&lang=fr');
+  }
+}
